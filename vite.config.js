a194d3339382cd9544e4756c5dc4a001e5dac217
Tushar <tushar432seervi@gmail.com>
2025-12-24 10:45:26 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: process.env.VITE_BASE_PATH || '/tushar_3D_portfolio',
+  base: process.env.VITE_BASE_PATH || '/',
 })
